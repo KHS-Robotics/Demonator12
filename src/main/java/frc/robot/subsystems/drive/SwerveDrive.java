1723206fbefb7301c3e85b46474bee0be7de3aa7
Canuck1 <jowettpayne@gmail.com>
2023-02-09 15:33:50 -0500

    new swerve positions

--- src/main/java/frc/robot/subsystems/drive/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveDrive.java
@@ -37,10 +37,10 @@ public class SwerveDrive extends SubsystemBase {
   public static double offset;
   public Pose2d startingPose;
   private PIDController targetPid;
-  private final Translation2d frontLeftLocation = new Translation2d(0.29845, 0.29845);
-  private final Translation2d frontRightLocation = new Translation2d(0.29845, -0.29845);
-  private final Translation2d rearLeftLocation = new Translation2d(-0.29845, 0.29845);
-  private final Translation2d rearRightLocation = new Translation2d(-0.29845, -0.29845);
+  private final Translation2d frontLeftLocation = new Translation2d(0.327025, 0.2693162);
+  private final Translation2d frontRightLocation = new Translation2d(0.327025, -0.2693162);
+  private final Translation2d rearLeftLocation = new Translation2d(-0.327025, 0.2693162);
+  private final Translation2d rearRightLocation = new Translation2d(-0.327025, -0.2693162);
 
   public PhotonWrapper photonCamera;
 
