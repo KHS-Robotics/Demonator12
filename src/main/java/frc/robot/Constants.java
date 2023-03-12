fdf58ad0b43ff6107e33f5359d29f71bc92c1e73
Jack Cammarata <jackcammarata1@gmail.com>
2023-03-12 15:08:27 -0400

    combine pid and ff for all swerves

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,68 +31,22 @@ public class Constants {
 
   public static final double SENS = 0.5;
 
-  public static final double FRONT_LEFT_P = 0.01;
-  public static final double FRONT_LEFT_I = 0.0;
-  public static final double FRONT_LEFT_D = 0.0001;
-
-  public static final double FRONT_RIGHT_P = 0.01;
-  public static final double FRONT_RIGHT_I = 0.0;
-  public static final double FRONT_RIGHT_D = 0.0001;
-
-  public static final double REAR_LEFT_P = 0.01;
-  public static final double REAR_LEFT_I = 0.0;
-  public static final double REAR_LEFT_D = 0.0001;
-
-  public static final double REAR_RIGHT_P = 0.01;
-  public static final double REAR_RIGHT_I = 0.0;
-  public static final double REAR_RIGHT_D = 0.0001;
-
-  public static final double FRONT_LEFT_KS = 2.2064E-06;
-  public static final double FRONT_LEFT_KV = 0.0;
-  public static final double FRONT_LEFT_KA = 0.0;
-
-  public static final double FRONT_RIGHT_KS = 2.2064E-06;
-  public static final double FRONT_RIGHT_KV = 0.0;
-  public static final double FRONT_RIGHT_KA = 0.0;
-
-  public static final double REAR_LEFT_KS = 2.2064E-06;
-  public static final double REAR_LEFT_KV = 0.0;
-  public static final double REAR_LEFT_KA = 0.0;
-
-  public static final double REAR_RIGHT_KS = 2.2064E-06;
-  public static final double REAR_RIGHT_KV = 0.0;
-  public static final double REAR_RIGHT_KA = 0.0;
-
   public static final double DRIVE_VEL_ENCODER = 0.000637;
   public static final double DRIVE_POS_ENCODER = 0.038318;
 
-  public static final double FRONT_RIGHT_DRIVE_P = 0.01;
-  public static final double FRONT_RIGHT_DRIVE_I = 0;
-  public static final double FRONT_RIGHT_DRIVE_D = 0;
-  public static final double FRONT_RIGHT_DRIVE_KS = 0.11408;
-  public static final double FRONT_RIGHT_DRIVE_KV = 3.2717;
-  public static final double FRONT_RIGHT_DRIVE_KA = 0.17904;
+  public static final double PIVOT_P = 0.01;
+  public static final double PIVOT_I = 0.0;
+  public static final double PIVOT_D = 0.0001;
+  public static final double PIVOT_KS = 2.2064E-06;
+  public static final double PIVOT_KV = 0.0;
+  public static final double PIVOT_KA = 0.0;
 
-  public static final double FRONT_LEFT_DRIVE_P = 0.01;
-  public static final double FRONT_LEFT_DRIVE_I = 0;
-  public static final double FRONT_LEFT_DRIVE_D = 0;
-  public static final double FRONT_LEFT_DRIVE_KS = 0.11408;
-  public static final double FRONT_LEFT_DRIVE_KV = 3.2717;
-  public static final double FRONT_LEFT_DRIVE_KA = 0.17904;
-
-  public static final double REAR_RIGHT_DRIVE_P = 0.01;
-  public static final double REAR_RIGHT_DRIVE_I = 0;
-  public static final double REAR_RIGHT_DRIVE_D = 0;
-  public static final double REAR_RIGHT_DRIVE_KS = 0.11408;
-  public static final double REAR_RIGHT_DRIVE_KV = 3.2717;
-  public static final double REAR_RIGHT_DRIVE_KA = 0.17904;
-
-  public static final double REAR_LEFT_DRIVE_P = 0.01;
-  public static final double REAR_LEFT_DRIVE_I = 0;
-  public static final double REAR_LEFT_DRIVE_D = 0;
-  public static final double REAR_LEFT_DRIVE_KS = 0.11408;
-  public static final double REAR_LEFT_DRIVE_KV = 3.2717;
-  public static final double REAR_LEFT_DRIVE_KA = 0.17904;
+  public static final double DRIVE_P = 0.01;
+  public static final double DRIVE_I = 0;
+  public static final double DRIVE_D = 0;
+  public static final double DRIVE_KS = 0.11408;
+  public static final double DRIVE_KV = 3.2717;
+  public static final double DRIVE_KA = 0.17904;
 
   public static final double ARM_P = 0.5;
   public static final double ARM_I = 0.0;
