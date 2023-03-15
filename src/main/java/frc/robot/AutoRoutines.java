0107fc96f09b68d2d681ec4f8bc17afc4a20e5b7
Jack Cammarata <jackcammarata1@gmail.com>
2023-03-15 19:11:34 -0400

    manual auto building

---
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -0,0 +1,74 @@
+package frc.robot;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ProxyCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.commands.drive.CenterSwerveModules;
+import frc.robot.commands.drive.balance.BalanceSequence;
+import frc.robot.commands.grabber.SetGrabber;
+
+public class AutoRoutines {
+    private static Command ScoreAngle = RobotContainer.arm.goToPivotLength(0.75, Constants.MIN_LENGTH);
+
+    public static Command getPlace1MobilityCableProtector() {
+        var traj = PathPlanner.loadPath("Place + Mobility (Cable Protector)", new PathConstraints(3, 2));
+
+        return new SequentialCommandGroup(
+            new CenterSwerveModules(false),
+            new InstantCommand(() -> RobotContainer.swerveDrive.setPose(traj.getInitialPose())),
+            new ProxyCommand(() -> RobotContainer.arm.goToSetpointScore(Constants.HIGH_POS)),
+            new SetGrabber(false),
+            ScoreAngle,
+            getTrajectoryCommand(traj)
+        );
+    }
+
+    public static Command getPlace1MobilityLoadingStation() {
+        var traj = PathPlanner.loadPath("Place + Mobility (Loading Station)", new PathConstraints(3, 2));
+        
+        return new SequentialCommandGroup(
+            new CenterSwerveModules(false),
+            new InstantCommand(() -> RobotContainer.swerveDrive.setPose(traj.getInitialPose())),
+            new ProxyCommand(() -> RobotContainer.arm.goToSetpointScore(Constants.HIGH_POS)),
+            new SetGrabber(false),
+            ScoreAngle,
+            getTrajectoryCommand(traj)
+        );
+    }
+
+    public static Command getPlace1BalanceSequenceCenter() {
+        var traj = PathPlanner.loadPath("Place + Engage", new PathConstraints(3, 2));
+
+        return new SequentialCommandGroup(
+            new CenterSwerveModules(false),
+            new InstantCommand(() -> RobotContainer.swerveDrive.setPose(traj.getInitialPose())),
+            new ProxyCommand(() -> RobotContainer.arm.goToSetpointScore(Constants.HIGH_POS)),
+            new SetGrabber(false),
+            ScoreAngle,
+            getTrajectoryCommand(traj),
+            new BalanceSequence(0)
+        );
+    }
+
+    private static Command getTrajectoryCommand(Trajectory trajectory) {
+        return new SwerveControllerCommand(
+            trajectory, 
+            RobotContainer.swerveDrive::getPose, 
+            RobotContainer.swerveDrive.kinematics, 
+            new PIDController(4, 0, 0),
+            new PIDController(4, 0, 0),
+            new ProfiledPIDController(0.4, 0, 0, new TrapezoidProfile.Constraints(1, 1)),
+            RobotContainer.swerveDrive::setModuleStates, 
+            RobotContainer.swerveDrive
+        );
+    }
+}
