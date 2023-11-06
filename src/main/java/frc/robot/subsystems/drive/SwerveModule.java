6c92a20bd28063db385872e2cfc7f0b45f67dbc8
jackc <jackcammarata1@gmail.com>
2023-11-06 09:59:20 -0500

    remove something?

--- src/main/java/frc/robot/subsystems/drive/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModule.java
@@ -21,7 +21,6 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
